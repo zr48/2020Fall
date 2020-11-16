1a2a503bc6a69d7b92f10c3f402960363f59fd90
zr48 <70662032+zr48@users.noreply.github.com>
2020-11-16 08:45:05 -0500

    Add files via upload

---
+++ lex/cmake-build-debug/CMakeFiles/lex.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lex.dir/main.cpp.obj"
+  "lex.exe"
+  "lex.exe.manifest"
+  "lex.lib"
+  "lex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
